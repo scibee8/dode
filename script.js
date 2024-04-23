822e58bf8e67dae336f61ce2199693b8e28e4bfa
Ryan K <136540593+scibee8@users.noreply.github.com>
2024-04-23 14:24:38 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+window.onload = () => {
+    let testEntityAdded = false;
+
+    const el = document.querySelector("[gps-new-camera]");
+
+    el.addEventListener("gps-camera-update-position", e => {
+        if(!testEntityAdded) {
+            alert(`Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`);
+            // Add a box to the north of the initial GPS position
+            const entity = document.createElement("a-entity");
+            entity.setAttribute('gltf-model', 'url(./assets/dode/scene.gltf)');
+            entity.setAttribute("scale", {
+                x: 20, 
+                y: 20,
+                z: 20
+            });
+            
+            entity.setAttribute('gps-new-entity-place', {
+                latitude: e.detail.position.latitude + 0.001,
+                longitude: e.detail.position.longitude
+            });
+            document.querySelector("a-scene").appendChild(entity);
+        }
+        testEntityAdded = true;
+    });
+};
